4fa2826bc24fc0f327937c17fb20b2bcffe1069c
Kal-Tenh <timyretro8@gmail.com>
2026-09-20 20:14:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -111,7 +111,6 @@ app.post('/v1/chat/completions', async (req, res) => {
     'Content-Type': 'application/json'
   },
   responseType: stream ? 'stream' : 'json',
-  timeout: 120000
 });
     
     if (stream) {
