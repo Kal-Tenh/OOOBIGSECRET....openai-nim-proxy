5db30197d71ece3e8ad9f88ab148658c512bbc9a
Kal-Tenh <timyretro8@gmail.com>
2026-09-21 13:13:46 +0700

    Update server.js
    deleting some models

--- server.js
+++ server.js
@@ -26,10 +26,8 @@ const MODEL_MAPPING = {
   'glm-5.3': 'z-ai/glm-5.3',
   'glm-5.3-flash': 'z-ai/glm-5-3-flash',
   'gpt-3.5-turbo': 'deepseek-ai/deepseek-v4-flash-0731',
-  'claude-3-opus': 'nvidia/nemotron-3-super-120b-a12b',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking',
   'claude-3-sonnet': 'deepseek-v4-pro-0813',
-  'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking',
   'kimi-k3': 'moonshotai/kimi-k3'
 };
 
